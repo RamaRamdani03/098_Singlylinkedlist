c6afb1a101704fb58055809e1be4f3c65003afa5
RamaRamdani03 <ramaramdani449@gamil.com>
2023-05-19 17:44:48 +0700

    membuat class list dengan hak access public

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -9,3 +9,16 @@ public:
 	char nama[20];
 	Node* next;
 };
+
+class list
+{
+	Node* START;
+public:
+	list();
+	void addNode();
+	bool Search(int nim, Node** current, Node** previous);
+	bool listEmpty();
+	bool delNode(int element);
+	bool Search(int nim, Node** previous, Node** current);
+	void traverse();
+};
