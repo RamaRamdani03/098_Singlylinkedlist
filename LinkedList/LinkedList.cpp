ebbd5890d1f1b8874ed80951f0845cbe2b659ebb
RamaRamdani03 <ramaramdani449@gamil.com>
2023-05-19 18:22:56 +0700

    untuk menghapus node dari daftar list

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -80,3 +80,15 @@ bool list::listEmpty()
 	else
 		return false;
 }
+bool list::delNode(int nim)/*Menghapus node dari dalam list*/
+{
+	Node* current, * previous;
+	if (Search(nim, &previous, &current) == false)
+		return false;
+	previous->next = current->next;
+	if (current == START)
+		START = START->next;
+
+	delete current;
+	return true;
+}
